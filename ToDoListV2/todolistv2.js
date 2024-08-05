d4ff3c1ed32f00da627ae09c5d7edd8589f672da
YacineBoukliff <y.bouklif@it-students.fr>
2024-08-05 09:12:02 +0200

    Ajout LocalStorage?

--- ToDoListV2/todolistv2.js
+++ ToDoListV2/todolistv2.js
@@ -1,34 +1,34 @@
 const inputBox = document.getElementById("input-box");
 const divListe = document.getElementById("div-list");
-const bouton = document.querySelector('.bouton');
+const bouton = document.querySelector(".bouton");
 const poubelle = document.querySelector(".supprimetout");
 
-bouton.addEventListener('click', ajoutertache);
+bouton.addEventListener("click", ajoutertache);
 
-function ajoutertache(){
-  if(inputBox.value.trim() === ""){
+function ajoutertache() {
+  if (inputBox.value.trim() === "") {
     alert("Rentrez une tache a faire !!!");
   } else {
-    let li = document.createElement('li');
+    let li = document.createElement("li");
     li.textContent = inputBox.value;
     divListe.appendChild(li);
-    
-    let span = document.createElement('span');
+
+    let span = document.createElement("span");
     span.textContent = "\u00d7";
     li.appendChild(span);
-    
+
     savedata();
     PoubelleaJour(); // Appel ici
   }
   inputBox.value = "";
 }
 
-function GererLesTaches(){
-  divListe.addEventListener('click', (e) => {
-    if (e.target.tagName === 'LI') {
-      e.target.classList.toggle('checked');
+function GererLesTaches() {
+  divListe.addEventListener("click", (e) => {
+    if (e.target.tagName === "LI") {
+      e.target.classList.toggle("checked");
       savedata();
-    } else if (e.target.tagName === 'SPAN') {
+    } else if (e.target.tagName === "SPAN") {
       if (confirm("Êtes-vous sûr de vouloir supprimer cette tâche ?")) {
         e.target.parentElement.remove();
         savedata();
@@ -37,17 +37,17 @@ function GererLesTaches(){
     }
   });
 
-  inputBox.addEventListener('keypress', (e) => {
-    if(e.key === "Enter"){
+  inputBox.addEventListener("keypress", (e) => {
+    if (e.key === "Enter") {
       ajoutertache();
     }
   });
 }
 
 function SupprimerTouteTache() {
-  poubelle.addEventListener('click', (e) => {
+  poubelle.addEventListener("click", (e) => {
     if (confirm("Êtes-vous sûr de vouloir supprimer TOUTES les tâches ?")) {
-      divListe.innerHTML = '';
+      divListe.innerHTML = "";
       savedata();
       PoubelleaJour(); // Appel ici
     }
@@ -58,12 +58,12 @@ function PoubelleaJour() {
   poubelle.hidden = divListe.children.length === 0;
 }
 
-function savedata(){
-  localStorage.setItem('data', divListe.innerHTML);
+function savedata() {
+  localStorage.setItem("data", divListe.innerHTML);
 }
 
-function recupererdata(){
-  divListe.innerHTML = localStorage.getItem('data');
+function recupererdata() {
+  divListe.innerHTML = localStorage.getItem("data");
   PoubelleaJour(); // Appel ici
 }
 
